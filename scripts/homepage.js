1abe04039b28f180eec0c059e085c4da16861a97
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-03-25 10:47:34 +0100

    Navbar implemented

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,3 +1,5 @@
 const allIssues = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
 let singleIssue = "https://phi-lab-server.vercel.app/api/v1/lab/issue/";
 let searchIssue = "https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=";
+
+document.getElementById("")
