9c42e877d606dbd6fa1370ac0208bbdba710c56c
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-03-25 15:39:34 +0100

    Added better spacing inside the cards, need to refactor the displayIssues() using filter()

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -16,33 +16,34 @@ const loadAllIssues = async () => {
 function displayIssues(issues, status = "all") {
     const container = document.getElementById("issue_container");
     container.innerHTML = "";
-
-    openIssues.length = 0;
-    closedIssues.length = 0;
-
-    issues.forEach(issue => {
-        if (issue.status === 'open') {
-            openIssues.push(issue);
-        } else if (issue.status === 'closed') {
-            closedIssues.push(issue);
-        }
-    }
-    )
-    if(status === "open"){
-        openIssues.forEach(issue => {
-            container.appendChild(createIssueCard(issue));
-        })
-    }
-    else if(status === "closed"){
-        closedIssues.forEach(issue => {
-            container.appendChild(createIssueCard(issue));
-        })
-    }
-    else{
+    if (status === "all") {
         issues.forEach(issue => {
             container.appendChild(createIssueCard(issue))
-        })
-    }
+        });
+    } else {
+        // Refactor array using filter
+        openIssues.length = 0;
+        closedIssues.length = 0;
+
+        issues.forEach(issue => {
+            if (issue.status === 'open') {
+                openIssues.push(issue);
+            } else if (issue.status === 'closed') {
+                closedIssues.push(issue);
+            }
+        }
+        )
+        if (status === "open") {
+            openIssues.forEach(issue => {
+                container.appendChild(createIssueCard(issue));
+            })
+        }
+        else if (status === "closed") {
+            closedIssues.forEach(issue => {
+                container.appendChild(createIssueCard(issue));
+            })
+        };
+    };
 }
 
 function createIssueCard(issue) {
@@ -55,18 +56,18 @@ function createIssueCard(issue) {
             <img src="${issue.status === "open" ? "./assets/Open-Status.png" : "./assets/Closed-Status.png"}" alt="image describing the state of the task">
             <p class="text-${priorityColor}-500 bg-${priorityColor}-300 rounded-xl text-center p-2">${issue.priority.toUpperCase()}</p>
         </div>
-        <h6 class="font-semibold text-xl text-black">${issue.title}</h6>
-        <p class="text-secondary">${issue.description}</p>
+        <h6 class="font-semibold text-xl text-black h-10 mb-4">${issue.title}</h6>
+        <p class="text-secondary h-25">${issue.description}</p>
         <div class="flex gap-1 justify-start items-center">
             <button class="flex gap-1 justify-center items-center border border-red-300 rounded-xl p-2"><i class="fa-solid fa-bug"></i> <span class="text-red-500">${issue.labels[0]}</span></button>
-            ${issue.labels[1]? `<button class="flex gap-1 justify-center items-center border border-yellow-300 rounded-xl p-2"><span class="text-yellow-500">${issue.labels[1]}</span></button>`:""}
+            ${issue.labels[1] ? `<button class="flex gap-1 justify-center items-center border border-yellow-300 rounded-xl p-2"><span class="text-yellow-500">${issue.labels[1]}</span></button>` : ""}
             
         </div>
         <hr class="text-secondary">
         <p class="text-secondary">#${issue.id} by ${issue.author}</p>
         <p class="text-secondary">${issue.createdAt}</p>
         `;
-    card.classList.add("bg-white", "flex", "flex-col", "gap-2","rounded-2xl", "shadow-sm","pt-.9", "pb-2", "px-5", "space-y-2");
+    card.classList.add("bg-white", "flex", "flex-col", "gap-2", "rounded-2xl", "shadow-sm", "pt-.9", "pb-2", "px-5", "space-y-2");
     return card;
 }
 
